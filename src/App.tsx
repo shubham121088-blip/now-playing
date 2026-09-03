cdb9b0662e65bdf8a99db171835595c308f2c0f5
Shubham Jha <shubhamjha@Kamleshs-MacBook-Air.local>
2026-09-03 12:10:04 +0530

    Overwrite App.tsx and fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-import { NowPlaying } from './components/NowPlaying';
-
-export function App() {
-  return <NowPlaying />;
-}
-
-export default App;
-
 import FullscreenButton from './components/FullscreenButton';
 
 export default function App() {
